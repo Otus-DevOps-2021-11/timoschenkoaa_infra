492d9db361434fd5f908ea64c37a69725cbdb4b8
Aleksandr Timoschenko <timoschenkoaa@okbtsp.com>
2022-02-09 17:01:43 +0300

    add main.tf

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -1,10 +1,3 @@
-provider "yandex" {
-  service_account_key_file = "var.service-account-key-file"
-  cloud_id                 = "var.cloud-id"
-  folder_id                = "var.folder-id"
-  zone                     = "var.zone-id"
-}
-
 resource "yandex_storage_bucket" "otus-storage-bucket" {
   bucket        = "bucket-name"
   access_key    = "access-key"
