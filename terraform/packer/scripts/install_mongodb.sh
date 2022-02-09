94bdec69ad1f1dbdfc2f4d0bb63b99bb1ec95343
Aleksandr Timoschenko <timoschenkoaa@okbtsp.com>
2022-02-09 16:25:39 +0300

    last fix

--- terraform/packer/scripts/install_mongodb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-apt install -y curl gnupg2 wget unzip
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.2.list
-
-apt-get update
-apt-get install -y mongodb-org
-systemctl start mongod
-systemctl enable mongod
--- terraform/packer/scripts/install_ruby.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-apt update
-sleep 1m
-apt install -y ruby-full ruby-bundler build-essential
