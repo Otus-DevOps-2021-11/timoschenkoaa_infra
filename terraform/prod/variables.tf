aef07c044e0880ea2c0feb6b90a23452c6838240
Aleksandr Timoschenko <timoschenkoaa@okbtsp.com>
2022-02-10 10:19:05 +0300

    fix

--- terraform/prod/variables.tf
+++ terraform/prod/variables.tf
@@ -6,11 +6,9 @@ variable "folder_id" {
 }
 variable "zone" {
   description = "Zone"
-  # Значение по умолчанию
   default = "ru-central1-a"
 }
 variable "public_key_path" {
-  # Описание переменной
   description = "Path to the public key used for ssh access"
 }
 variable "image_id" {
